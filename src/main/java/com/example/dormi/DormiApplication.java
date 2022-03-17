d5bee98fde4643ba801d1a66f15a4287089f953f
hyena0608 <goatsfish@gmail.com>
2022-03-17 17:57:18 +0900

    init

---
+++ src/main/java/com/example/dormi/DormiApplication.java
@@ -0,0 +1,13 @@
+package com.example.dormi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DormiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DormiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dormi/DormiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dormi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DormiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
