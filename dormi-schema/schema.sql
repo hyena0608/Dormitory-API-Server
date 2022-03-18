3f836edb02ece075cde2bd34f7bdbf7720f48742
hyena0608 <goatsfish@gmail.com>
2022-03-18 13:36:52 +0900

    chore(오타): 수정할 때 다 바꾸지 않은거 다시 수정

--- dormi-schema/schema.sql
+++ dormi-schema/schema.sql
@@ -8,7 +8,8 @@ CREATE TABLE student (
   student_name VARCHAR(64) NOT NULL,
   student_age INT NOT NULL,
   student_start_dt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  student_delete_dt TIMESTAMP NOT NULL
+  student_update_dt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  student_delete_dt TIMESTAMP NOT NULL DEFAULT ''
 );
 
 select "dormitory";
@@ -18,7 +19,8 @@ CREATE TABLE dormitory (
   dormitory_name VARCHAR(64) NOT NULL,
   dormitory_address VARCHAR(64) NOT NULL,
   dormitory_start_dt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  dormitory_delete_dt TIMESTAMP NOT NULL
+  dormitory_update_dt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  dormitory_delete_dt TIMESTAMP NOT NULL DEFAULT ''
 );
 
 select "room";
@@ -27,9 +29,10 @@ CREATE TABLE room (
   room_id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   room_number BIGINT NOT NULL,
   room_limit_cnt INT NOT NULL,
-  room_current_cnt INT NOT NULL,
+  room_current_cnt INT NOT NULL DEFAULT 0,
   room_start_dt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  room_delete_dt TIMESTAMP NOT NULL,
+  room_update_dt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  room_delete_dt TIMESTAMP NOT NULL DEFAULT '',
   dormitory_id BIGINT NOT NULL
 );
 
@@ -37,9 +40,11 @@ select "dormitory_student";
 
 CREATE TABLE dormitory_student (
   dormitory_student_id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  semester INT NOT NULL,
-  dormitory_start_dt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  dormitory_delete_dt TIMESTAMP NOT NULL,
+  dormitory_student_semester INT NOT NULL,
+  dormitory_student_start_dt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  dormitory_student_update_dt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  dormitory_student_delete_dt TIMESTAMP NOT NULL DEFAULT '',
   dormitory_id BIGINT NOT NULL,
+  room_id BIGINT NOT NULL,
   student_id BIGINT NOT NULL
 );
