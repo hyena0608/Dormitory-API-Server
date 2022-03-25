a8ee28c279f8d9c625517d5762775fc64f4576e5
hyena0608 <goatsfish@gmail.com>
2022-03-25 11:09:40 +0900

    refactor: Point 로직 예외처리 ê기능 구현

--- src/main/java/com/example/dormi/controller/handler/InsertPointToStudentHandler.java
+++ src/main/java/com/example/dormi/controller/handler/InsertPointToStudentHandler.java
@@ -3,14 +3,19 @@ package com.example.dormi.controller.handler;
 import com.example.dormi.controller.request.*;
 import com.example.dormi.controller.response.*;
 import com.example.dormi.mapper.DormiMapper;
+import com.example.dormi.mapper.vo.DormitoryStudentInfoVo;
+import com.example.dormi.mapper.vo.ManagerInfoVo;
 import com.example.dormi.mapper.vo.PointInfoVo;
+import com.example.dormi.mapper.vo.RuleInfoVo;
 import com.example.dormi.service.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import com.example.dormi.controller.ResultCode;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -34,39 +39,45 @@ public class InsertPointToStudentHandler extends BaseHandler {
 
         try {
 
-            if (!isAllExists(dormitoryStudentId, ruleId, managerId)) {
-                res.setCode(ResultCode.Failed);
-                return res;
-            }
+            isStudentDangerousPoint(dormitoryStudentId);
 
-            int totalPoint = getStudentTotalPoint(dormitoryStudentId);
-            if (totalPoint <= -60) {
-                res.setCode(ResultCode.Failed);
-                return res;
-            }
+            isAllExist(dormitoryStudentId, ruleId, managerId);
 
             long pointId = mapper.insertPointToStudent(dormitoryStudentId, ruleId, managerId);
             res.setPointId(pointId);
             res.setCode(ResultCode.Success);
-            return res;
 
         } catch (Exception e) {
             log.error(e.toString());
             res.setCode(ResultCode.Failed);
             return res;
         }
+        return res;
     }
 
-    private int getStudentTotalPoint(long dormitoryStudentId) {
+    private void isAllExist(long dormitoryStudentId, long ruleId, long managerId)  {
+        Optional<ManagerInfoVo> findManager = Optional.ofNullable(mapper.selectManagerByManagerId(managerId));
+        if (findManager.isEmpty()) {
+            throw new NullPointerException("해당 관리자가 존재하지 않습니다 !");
+        }
+
+        Optional<DormitoryStudentInfoVo> findDormitoryStudent = Optional.ofNullable(mapper.selectDormitoryStudentByDormitoryStudentId(dormitoryStudentId));
+        if (findDormitoryStudent.isEmpty()) {
+            throw new NullPointerException("해당 기숙 학생이 존재하지 않습니다 !");
+        }
+
+        Optional<RuleInfoVo> findRule = Optional.ofNullable(mapper.selectRule(ruleId));
+        if (findRule.isEmpty()) {
+            throw new NullPointerException("해당 규칙이 존재하지 않습니다 !");
+        }
+    }
+
+    private void isStudentDangerousPoint(long dormitoryStudentId) {
         List<PointInfoVo> pointInfoVoList = mapper.selectPoint(dormitoryStudentId);
         int answer = pointInfoVoList.stream().mapToInt(o -> (int) o.getPointId()).sum();
-        return answer;
+        if (answer <= -60) {
+            throw new IllegalArgumentException("이 학생은 벌점 -60점을 넘었습니다. 퇴실해야 합니다.");
+        }
     }
 
-    private boolean isAllExists(long dormitoryStudentId, long ruleId, long managerId) {
-        return mapper.selectManagerByManagerId(managerId).getManagerId() == managerId &&
-                mapper.selectDormitoryStudentByDormitoryStudentId(dormitoryStudentId)
-                        .getDormitoryStudentId() == dormitoryStudentId &&
-                mapper.selectRule(ruleId).getRuleId() == ruleId;
-    }
 }
